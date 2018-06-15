baaf2dcb575858d72bafb8cc31940181d5f7cff2
Henque Mateus Dalfovo Campos <hmdcampos@gmail.com>
2018-06-15 17:09:16 -0300

    Minha mecanica

---
+++ MinhaMecanica/WFA/ListaCarro.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace WFA
+{
+    partial class ListaCarro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.NomeColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MarcaColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AnoFabricacaoColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ValorColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPageLista = new System.Windows.Forms.TabPage();
+            this.tabPageCadastro = new System.Windows.Forms.TabPage();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAno = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMarca = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnApagar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPageLista.SuspendLayout();
+            this.tabPageCadastro.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NomeColumn1,
+            this.MarcaColumn1,
+            this.AnoFabricacaoColumn1,
+            this.ValorColumn1});
+            this.dataGridView1.Location = new System.Drawing.Point(3, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(445, 266);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // NomeColumn1
+            // 
+            this.NomeColumn1.HeaderText = "Nome";
+            this.NomeColumn1.Name = "NomeColumn1";
+            this.NomeColumn1.ReadOnly = true;
+            // 
+            // MarcaColumn1
+            // 
+            this.MarcaColumn1.HeaderText = "Marca";
+            this.MarcaColumn1.Name = "MarcaColumn1";
+            this.MarcaColumn1.ReadOnly = true;
+            // 
+            // AnoFabricacaoColumn1
+            // 
+            this.AnoFabricacaoColumn1.HeaderText = "Ano Fabricação";
+            this.AnoFabricacaoColumn1.Name = "AnoFabricacaoColumn1";
+            this.AnoFabricacaoColumn1.ReadOnly = true;
+            // 
+            // ValorColumn1
+            // 
+            this.ValorColumn1.HeaderText = "Valor";
+            this.ValorColumn1.Name = "ValorColumn1";
+            this.ValorColumn1.ReadOnly = true;
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdicionar.Location = new System.Drawing.Point(354, 3);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(94, 33);
+            this.btnAdicionar.TabIndex = 1;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPageLista);
+            this.tabControl1.Controls.Add(this.tabPageCadastro);
+            this.tabControl1.ItemSize = new System.Drawing.Size(130, 20);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(462, 338);
+            this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPageLista
+            // 
+            this.tabPageLista.Controls.Add(this.btnEditar);
+            this.tabPageLista.Controls.Add(this.dataGridView1);
+            this.tabPageLista.Controls.Add(this.btnApagar);
+            this.tabPageLista.Controls.Add(this.btnAdicionar);
+            this.tabPageLista.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabPageLista.Location = new System.Drawing.Point(4, 24);
+            this.tabPageLista.Name = "tabPageLista";
+            this.tabPageLista.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageLista.Size = new System.Drawing.Size(454, 310);
+            this.tabPageLista.TabIndex = 0;
+            this.tabPageLista.Text = "Lista";
+            this.tabPageLista.UseVisualStyleBackColor = true;
+            // 
+            // tabPageCadastro
+            // 
+            this.tabPageCadastro.Controls.Add(this.btnCancelar);
+            this.tabPageCadastro.Controls.Add(this.btnCadastrar);
+            this.tabPageCadastro.Controls.Add(this.txtValor);
+            this.tabPageCadastro.Controls.Add(this.label4);
+            this.tabPageCadastro.Controls.Add(this.txtAno);
+            this.tabPageCadastro.Controls.Add(this.label3);
+            this.tabPageCadastro.Controls.Add(this.txtMarca);
+            this.tabPageCadastro.Controls.Add(this.label2);
+            this.tabPageCadastro.Controls.Add(this.txtNome);
+            this.tabPageCadastro.Controls.Add(this.label1);
+            this.tabPageCadastro.Location = new System.Drawing.Point(4, 24);
+            this.tabPageCadastro.Name = "tabPageCadastro";
+            this.tabPageCadastro.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageCadastro.Size = new System.Drawing.Size(454, 310);
+            this.tabPageCadastro.TabIndex = 1;
+            this.tabPageCadastro.Text = "Cadastro";
+            this.tabPageCadastro.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(10, 220);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(125, 84);
+            this.btnCancelar.TabIndex = 9;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrar.Location = new System.Drawing.Point(322, 220);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(126, 84);
+            this.btnCadastrar.TabIndex = 8;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // txtValor
+            // 
+            this.txtValor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValor.Location = new System.Drawing.Point(10, 188);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(438, 26);
+            this.txtValor.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Valor";
+            // 
+            // txtAno
+            // 
+            this.txtAno.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAno.Location = new System.Drawing.Point(10, 136);
+            this.txtAno.Name = "txtAno";
+            this.txtAno.Size = new System.Drawing.Size(438, 26);
+            this.txtAno.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ano";
+            // 
+            // txtMarca
+            // 
+            this.txtMarca.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMarca.Location = new System.Drawing.Point(10, 84);
+            this.txtMarca.Name = "txtMarca";
+            this.txtMarca.Size = new System.Drawing.Size(438, 26);
+            this.txtMarca.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Marca";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(10, 32);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(438, 26);
+            this.txtNome.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.Location = new System.Drawing.Point(195, 6);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(66, 30);
+            this.btnEditar.TabIndex = 10;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnApagar
+            // 
+            this.btnApagar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApagar.Location = new System.Drawing.Point(267, 3);
+            this.btnApagar.Name = "btnApagar";
+            this.btnApagar.Size = new System.Drawing.Size(81, 33);
+            this.btnApagar.TabIndex = 11;
+            this.btnApagar.Text = "Apagar";
+            this.btnApagar.UseVisualStyleBackColor = true;
+            this.btnApagar.Click += new System.EventHandler(this.btnApagar_Click);
+            // 
+            // ListaCarro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 362);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "ListaCarro";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lista de Carros";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPageLista.ResumeLayout(false);
+            this.tabPageCadastro.ResumeLayout(false);
+            this.tabPageCadastro.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NomeColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MarcaColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AnoFabricacaoColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ValorColumn1;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageLista;
+        private System.Windows.Forms.TabPage tabPageCadastro;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAno;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMarca;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnApagar;
+        private System.Windows.Forms.Button btnEditar;
+    }
+}
+
---
+++ MinhaMecanica/WFA/ListaCarro.cs
@@ -0,0 +1,158 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA
+{
+    public partial class ListaCarro : Form
+    {
+        string nomeAntigo = string.Empty;
+        List<Carro> carros = new List<Carro>();
+
+        public ListaCarro()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedIndex = 1;
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Carro carro = new Carro();
+                {
+                    carro.Nome = txtNome.Text;
+                    carro.Marca = txtMarca.Text;
+                    carro.AnoFabricacao = Convert.ToInt16(txtAno.Text);
+                    carro.Valor = Convert.ToDecimal(txtValor.Text);
+                }
+                if (nomeAntigo == "")
+                {
+                    carros.Add(carro);
+                    AdicionarCarroATabela(carro);
+                }
+
+                else
+                {
+                    /*for (int i = 0; i < carros.Count(); i++)
+                    {
+                        Carro carroAux = carros[i];
+                        if (carroAux.Nome == nomeAntigo)
+                        {
+                            carros[i] = carro;
+                        }
+                    }*/
+
+                    int linha = carros.FindIndex(x => x.Nome == nomeAntigo);
+                    carros[linha] = carro;
+                    EditarCarroNaTebela(carro, linha);
+                    MessageBox.Show("Alterado com sucesso");
+                    nomeAntigo = string.Empty;
+                }
+                tabControl1.SelectedIndex = 0;
+                LimparCampos();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void EditarCarroNaTebela(Carro carro, int linha)
+        {
+            dataGridView1.Rows[linha].Cells[0].Value = carro.Nome;
+            dataGridView1.Rows[linha].Cells[1].Value = carro.Marca;
+            dataGridView1.Rows[linha].Cells[2].Value = carro.AnoFabricacao;
+            dataGridView1.Rows[linha].Cells[3].Value = carro.Valor;
+        }
+
+        private void AdicionarCarroATabela(Carro carro)
+        {
+            dataGridView1.Rows.Add(new Object[]{
+                carro.Nome, carro.Marca, carro.AnoFabricacao, carro.Valor
+            });
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+        }
+
+        private void LimparCampos()
+        {
+            txtNome.Text = "";
+            txtMarca.Text = "";
+            txtAno.Text = "";
+            txtValor.Text = "";
+        }
+
+        private void btnApagar_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count == 0)
+            {
+                MessageBox.Show("Cadastre um carro");
+                tabControl1.SelectedIndex = 1;
+            }
+
+            if (dataGridView1.CurrentRow == null)
+            {
+                MessageBox.Show("Selecione uma linha");
+            }
+
+            int linhaSelecionada = dataGridView1.CurrentRow.Index;
+            string nome = dataGridView1.Rows[linhaSelecionada].Cells[0].Value.ToString();
+            for (int i = 0; i < carros.Count(); i++)
+            {
+                Carro carro = carros[i];
+                if (carro.Nome == nome)
+                {
+                    carros.RemoveAt(i);
+                }
+            }
+
+            //carros.Remove(carros.Find(x=> x.Nome == nome));
+
+            dataGridView1.Rows.RemoveAt(linhaSelecionada);
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count == 0)
+            {
+                MessageBox.Show("Cadastre um carro");
+                tabControl1.SelectedIndex = 1;
+            }
+
+            if (dataGridView1.CurrentRow == null)
+            {
+                MessageBox.Show("Selecione uma linha");
+            }
+
+            int linhaSelecionada = dataGridView1.CurrentRow.Index;
+            string nome = dataGridView1.Rows[linhaSelecionada].Cells[0].Value.ToString();
+
+            foreach (Carro carro in carros)
+            {
+                txtNome.Text = carro.Nome;
+                txtMarca.Text = carro.Marca;
+                txtAno.Text = Convert.ToString(carro.AnoFabricacao);
+                txtValor.Text = Convert.ToString(carro.Valor);
+                tabControl1.SelectedIndex = 1;
+                nomeAntigo = carro.Nome;
+                break;
+            }
+        }
+    }
+}
